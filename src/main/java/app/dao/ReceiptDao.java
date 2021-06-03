b4daf34fdf0d7ec275ecd84a158c7530a97051d2
euzLIsnY <euzlisny@email.com>
2021-06-03 16:02:00 +0300

    Receipt dao added

--- src/main/java/app/dao/ReceiptDao.java
+++ src/main/java/app/dao/ReceiptDao.java
@@ -1,2 +1,29 @@
-package app.dao;public class ReceiptDao {
+package app.dao;
+
+import app.entities.DBCPDataSource;
+import app.entities.Receipt;
+
+import java.sql.*;
+
+public class ReceiptDao {
+    public void addReceipt(Receipt rec) throws SQLException {
+        String INSERT_APPLICATION_SQL = "INSERT INTO receipt" +
+                "  (price, application_id) VALUES " +
+                " (?, ?);";
+        Connection con = DBCPDataSource.getConnection();
+        ResultSet rs;
+        try (PreparedStatement preparedStatement = con.prepareStatement(INSERT_APPLICATION_SQL, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setDouble(1, rec.getPrice());
+            preparedStatement.setInt(2, rec.getAppId());
+
+            if(preparedStatement.executeUpdate()>0) {
+                rs = preparedStatement.getGeneratedKeys();
+                if (rs.next()) {
+                    rec.setRecId(rs.getInt(1));
+                }
+            }
+        } catch (SQLException e) {
+            UserDao.printSQLException(e);
+        }
+    }
 }
--- src/main/java/app/entities/Receipt.java
+++ src/main/java/app/entities/Receipt.java
@@ -1,2 +1,28 @@
-package app.entities;public class Receipt {
+package app.entities;
+
+public class Receipt {
+    private int id;
+    private double price;
+    private int appId;
+
+    public Receipt(double price, int appId){
+        this.price = price;
+        this.appId = appId;
+    }
+
+    public int getRecId() {
+        return id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getAppId() {
+        return appId;
+    }
+
+    public void setRecId(int id) {
+        this.id = id;
+    }
 }
